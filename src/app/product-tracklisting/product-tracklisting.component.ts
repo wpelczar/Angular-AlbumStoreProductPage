b005f1522790ca6f3e529d6e258f680ce9bae6a9
wpelczar <wpelczar1@gmail.com>
2018-02-09 22:20:17 +0100

    ehh

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Album } from 'app/album';
-import { ProductService } from 'app/product.service';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-tracklisting',
