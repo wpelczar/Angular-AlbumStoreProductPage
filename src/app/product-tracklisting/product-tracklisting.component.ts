2c7ced5531108735bf3acc30d994258f9f1ada97
wpelczar <wpelczar1@gmail.com>
2018-02-09 22:17:28 +0100

    Use ProductService in ProductTracklistingComponent

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from 'app/album';
+import { ProductService } from 'app/product.service';
 
 @Component({
   selector: 'app-product-tracklisting',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  constructor() { }
+  albumInfo: Album;
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
