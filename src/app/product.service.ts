3b731efa3998ee48d0c061aee7af2b5a75dfff82
wpelczar <wpelczar1@gmail.com>
2018-02-09 22:06:20 +0100

    Determined return type as Album interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -8,8 +10,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
-      .map((response) => response.json());
+      .map((response) => <Album>response.json());
   }
 }
